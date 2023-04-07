9511c7ec7cf512f30e17ec3f3f98eb23bacda0c9
Tomás Weigenast <tomaswegenast@gmail.com>
2023-04-07 20:37:38 -0300

    add out method to write to stdout

--- generator/src/index.ts
+++ generator/src/index.ts
@@ -1,18 +1,45 @@
 #!/usr/bin/env node
 import fs from 'fs'
-import { parseSnapshot } from './models'
+import { NexemaSnapshot, parseSnapshot } from './models'
 import { Generator } from './generator'
 
 function main() {
-    // const args = process.argv.slice(2)
+    const args = new Map(
+        process.argv
+            .slice(2)
+            .map((x) => x.split('='))
+            .map((x) => [x[0], x[1]])
+    )
     const content = fs.readFileSync(process.stdin.fd, 'utf-8')
-    const snapshot = parseSnapshot(content)
+    let snapshot: NexemaSnapshot
+    try {
+        snapshot = parseSnapshot(content)
+    } catch (err) {
+        out('{"error":"error-invalid-snapshot"}')
+        return
+    }
+
+    const outputPath = args.get('output-path')
+    if (!outputPath) {
+        out('{"error":"missing-output-path"}')
+        return
+    }
 
     const generator = new Generator(snapshot, {
-        outputPath: '',
+        outputPath: outputPath,
         useOnlyMaps: true,
     })
-    generator.run()
+    const result = generator.run()
+    if (result.exitCode) {
+        out(`{"error":"error-${result.exitCode}"}`)
+        return
+    }
+
+    out(JSON.stringify(result.files))
+}
+
+function out(content: string): void {
+    process.stdout.write(`${content}\n`)
 }
 
 main()
